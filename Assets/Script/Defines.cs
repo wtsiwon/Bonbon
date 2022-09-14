ed2676c100c392971f0aff4b8edcb379b4cbcb18
wtsiwon <siwon4727@gmail.com>
2022-09-14 18:50:37 +0900

    enum 정의

--- Assets/Script/Defines.cs
+++ Assets/Script/Defines.cs
@@ -10,7 +10,52 @@ public struct Stat
     public float perClickProduct;
 }
 
-public enum EDessert
+public struct DessertType
+{
+    
+}
+
+public enum EDessertType
+{
+    Cake,
+    Cookie,
+    Macaron,
+    ShavedIce,//ºù¼ö
+    Pudding,
+    Chocolate,
+    Bread,
+    RiceCake,
+    KoreanSweet,//ÇÑ°ú
+}
+
+public enum ECakeType
+{
+    Chocolate,
+    Cheese,
+    Strawberry,
+    Tiramisu,
+    Crepe,
+}
+
+public enum ECookieType
+{
+    Chocolate,
+    Meringue,//¸Ó·©
+    Fortune,
+    Oreo,
+    Butter,
+}
+
+public enum EMacaronType
+{
+    Honey,
+    Vanilla,
+    Caramel,
+    Coconut,
+    Pepper,//ÈÄÃß
+}
+
+public enum EShavedIceType
 {
 
 }
