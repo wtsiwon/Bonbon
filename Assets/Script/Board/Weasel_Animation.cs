5b1238190a4efbc612063900cf168366d24e61b8
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-09-21 13:33:06 +0900

    리스트를 통한 족제비 애니메이션 추가
    리스트를 통한 족제비 애니메이션 추가

---
+++ Assets/Script/Board/Weasel_Animation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weasel_Animation : MonoBehaviour
+{
+    public List<GameObject> Weasel_Management = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
