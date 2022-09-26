6b879da36317d263e663c4f49c2846adc45154ef
wtsiwon <siwon4727@gmail.com>
2022-09-27 00:01:15 +0900

    상속관계

--- Assets/Script/Elements/Element.cs
+++ Assets/Script/Elements/Element.cs
@@ -10,5 +10,36 @@ public class Element : MonoBehaviour
     [Tooltip("이름")]
     protected TextMeshProUGUI name;
 
+    [SerializeField]
+    [Tooltip("IconSprite")]
+    protected Sprite iconSprite;
+
+    [SerializeField]
+    [Tooltip("Sprite가 들어갈 Image")]
+    protected Image imageCom;
+
+    [SerializeField]
+    [Tooltip("클릭당 생산량")]
+    protected TextMeshProUGUI clickIncrementTxt;
+
+    [SerializeField]
+    [Tooltip("초당 생산량")]
+    protected TextMeshProUGUI secondIncrementTxt;
+
+    [SerializeField]
+    [Tooltip("비용")]
+    protected TextMeshProUGUI costTxt;
+
+
+
+    protected virtual void Update()
+    {
+        
+    }
+
+    private void ApplyTexts()
+    {
+
+    }
 
 }
